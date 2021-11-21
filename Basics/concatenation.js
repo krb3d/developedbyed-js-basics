894c1903423f11ad717373338ae23aa26b40a062
Sergey Speshylov <s.speshylov@go-bbg.com>
2021-11-21 22:59:32 +0200

    Chapter 1 - concat

---
+++ Basics/concatenation.js
@@ -0,0 +1,10 @@
+const bank1 = 100;
+const bank2 = 50;
+const allBanks = bank1 + bank2;
+
+const heSaid = "Steeve once said \"Hi!\".";
+const user = 'Sergey "Quote" S.';
+const inter = `User: ${user}`;
+
+// weird
+const s11 = 1 + '1';
---
+++ Basics/dataTypes.js
@@ -0,0 +1,29 @@
+// --
+// Primitive data types
+// --
+
+// Numbers
+const day = 5;
+
+// Strings
+const name = "Serhii";
+console.log(typeof day);
+
+// Boolean
+let isLogged = true;
+
+// Undefined
+let undef;
+console.log(undef);
+
+// Null
+let minute = null;
+console.log(minute);
+
+// Symbol
+const name = Symbol();
+
+// --
+// OBJECT
+// --
+let obj = {};
