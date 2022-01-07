fa70a01c93cccd99a546180222668f224bd71455
Sergey Speshylov <s.speshylov@go-bbg.com>
2022-01-08 00:42:46 +0200

    basic beats loop

--- 06_Beatmaker/app.js
+++ 06_Beatmaker/app.js
@@ -3,30 +3,54 @@ class DrumKit {
     this.pads = document.querySelectorAll(".pad");
     this.playBtn = document.querySelector(".play");
 
-    this.kickAudio = document.querySelector("kick-sound");
-    this.snareAudio = document.querySelector("snare-sound");
-    this.hihatAudio = document.querySelector("hihat-sound");
+    this.kickAudio = document.querySelector(".kick-sound");
+    this.snareAudio = document.querySelector(".snare-sound");
+    this.hihatAudio = document.querySelector(".hihat-sound");
 
     this.index = 0;
-    this.bpm = 60;
+    this.bpm = 180;
+
+    this.loop = undefined;
   }
 
   repeat() {
-    let step = this.index % 8;
+    let step = this.index % 13;
     if (this.index > 100) {
       this.index = step;
     }
+    this.index++;
 
     const activeBars = document.querySelectorAll(`.b${step}`);
-
-    this.index++;
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      if (bar.classList.contains("active")) {
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
+    });
   }
 
   start() {
-    const interval = (1000 * 60) / this.bpm;
-    setInterval(() => {
-      this.repeat();
-    }, interval);
+    if (this.loop) {
+      clearInterval(this.loop);
+      this.loop = undefined;
+      this.index = 0;
+    } else {
+      const interval = (1000 * 60) / this.bpm;
+      this.loop = setInterval(() => {
+        this.repeat();
+      }, interval);
+    }
   }
 
   activePad(event) {
@@ -42,4 +66,7 @@ drumKit.playBtn.addEventListener("click", function () {
 
 drumKit.pads.forEach((pad) => {
   pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
 });
