13b44996c8f1e4c5d44849da189d1521f6e288ae
Sergey Speshylov <s.speshylov@go-bbg.com>
2022-02-10 22:06:17 +0200

    add promises example

--- 09_AsyncJS/promise.js
+++ 09_AsyncJS/promise.js
@@ -2,7 +2,7 @@ console.log("Start");
 
 const prom = new Promise((resolve, rejext) => {
   setTimeout(() => {
-    if (Math.random() > 0.5) {
+    if (Math.random() > 0.2) {
       resolve({ user: "Mini Me" });
     } else {
       rejext(new Error("naaaa :("));
@@ -19,3 +19,33 @@ prom
   });
 
 console.log("Finish");
+
+// --------------------
+function loginUser(email, password) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("now we have data!");
+      resolve({ userEmail: email });
+    }, 300);
+  });
+}
+function getUserVideos(email) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(["v1", "v2", "v3"]);
+    }, 200);
+  });
+}
+
+function getVideosDetails(video) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`title ${video}`);
+    }, 200);
+  });
+}
+
+loginUser("Serhii", "p0ss")
+  .then((user) => getUserVideos(user))
+  .then((videos) => getVideosDetails(videos[0]))
+  .then((detail) => console.log(detail));
